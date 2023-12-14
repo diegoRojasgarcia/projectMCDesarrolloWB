23d3b39518c16afc16a8ae961ee331d20aa68216
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-12-14 09:40:02 -0300

    update

--- src/proyecto/dto/update-proyecto.input.ts
+++ src/proyecto/dto/update-proyecto.input.ts
@@ -3,8 +3,8 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 @InputType()
 export class updateProyectoDto {
   @Field({ nullable: true })
-  nombre: string;
+  nombre?: string;
 
   @Field({ nullable: true })
-  area: string;
+  area?: string;
 }
